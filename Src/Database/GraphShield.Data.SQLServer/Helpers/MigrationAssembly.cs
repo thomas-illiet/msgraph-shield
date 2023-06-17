ffe875eaff11991073d5198ffc1f2620075da794
Thomas ILLIET <contact@thomas-illiet.fr>
2023-06-17 23:42:27 +0200

    Add database providers

---
+++ Src/Database/GraphShield.Data.SQLServer/Helpers/MigrationAssembly.cs
@@ -0,0 +1,10 @@
+﻿namespace GraphShield.Data.SQLServer.Helpers
+{
+    /// <summary>
+    /// Helper class for migration assembly management.
+    /// </summary>
+    public sealed class MigrationAssembly
+    {
+        // No additional implementation is required for now.
+    }
+}
