f7382fd971eb5731cd204aa10821f283c69e320f
Thomas ILLIET <contact@thomas-illiet.fr>
2023-06-18 23:24:54 +0200

    Improve unauthorized exception

--- Src/Product/GraphShield.Proxy/Exceptions/UnauthorizedException.cs
+++ Src/Product/GraphShield.Proxy/Exceptions/UnauthorizedException.cs
@@ -23,5 +23,21 @@ namespace GraphShield.Proxy.Exceptions
             Type = type ?? "https://datatracker.ietf.org/doc/html/rfc7235#section-3.1";
             Title = title ?? "Unauthorized";
         }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnauthorizedException"/> class.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        /// <param name="extendedInfo">The extended information associated with the exception.</param>
+        /// <param name="type">The type of the exception.</param>
+        /// <param name="title">The title of the exception.</param>
+        public UnauthorizedException(Exception exception, string message, IDictionary<string, object?>? extendedInfo = null, string? type = null, string? title = null)
+            : base(exception, message)
+        {
+            Status = 401;
+            Extensions = extendedInfo ?? Extensions;
+            Type = type ?? "https://datatracker.ietf.org/doc/html/rfc7235#section-3.1";
+            Title = title ?? "Unauthorized";
+        }
     }
 }
